9e8b2215b3954e0624a70fc1e7b8c4b47fbdb812
Razvan <mocanurazvan92@yahoo.ro>
2020-04-10 19:18:54 +0300

    design overhaul

--- public/scripts/institutionDashBoard.js
+++ public/scripts/institutionDashBoard.js
@@ -1,55 +1,23 @@
+function changeLanguage(language) {
+  var element = document.getElementById("url");
+  element.value = language;
+  element.innerHTML = language;
+}
 
-$(document).ready(function () {
-    $("#modify_institution").hide();
-    $("#institution_members").hide();
-    $("#document_sending").hide();
-    $("#document_checking").hide();
-    $("#institution_info").show();
-    
-  $('#institutionInfo').click(function () {
+function showDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-    $("#modify_institution").hide();
-    $("#institution_members").hide();
-    $("#document_sending").hide();
-    $("#document_checking").hide();
-    $("#institution_info").show();
-  })
-  
-  $('#modifyInstitution').click(function () {
-
-    $("#institution_info").hide();
-    $("#institution_members").hide();
-    $("#document_sending").hide();
-    $("#document_checking").hide();
-    $("#modify_institution").show();
-    })
-
-    $('#institutionMembers').click(function () {
-
-	$("#institution_info").hide();
-    $("#modify_institution").hide();
-    $("#document_sending").hide();
-    $("#document_checking").hide();
-    $("#institution_members").show();
-    })
-    
-    $('#documentSending').click(function () {
-
-        $("#institution_info").hide();
-        $("#modify_institution").hide();
-        $("#institution_members").hide();
-        $("#document_sending").show();
-        $("#document_checking").hide();
-    })
-
-
-    $('#documentChecking').click(function () {
-      
-        $("#institution_info").hide();
-        $("#modify_institution").hide();
-        $("#institution_members").hide();
-        $("#document_sending").hide();
-        $("#document_checking").show();
-  })
-  
-});
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
+      }
+  }
+}
