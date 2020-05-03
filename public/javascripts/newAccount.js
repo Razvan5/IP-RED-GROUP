b05c3ae5547e0288f99d51314e3b0b75634d5220
Adee <test@test>
2020-05-03 19:01:31 +0300

    Integrat new Account+ alerte user-friendly la erori

--- public/javascripts/newAccount.js
+++ public/javascripts/newAccount.js
@@ -15,38 +15,49 @@
     }
 
     document.addEventListener("DOMContentLoaded", function () {
-        var form = document.getElementById("login-form");
+        var form = document.getElementById("register-form");
         var xmlhttp = new XMLHttpRequest();
 
         form.addEventListener("submit", function (e) {
             e.preventDefault();
+
+            var passwords={
+                first:document.getElementById("registerPassword"),
+                second:document.getElementById("psw-again")
+            };
+
+            if(passwords.first.value!=passwords.second.value){
+                alert("PASSWORDS DO NOT MATCH");
+            }else{
             var loginData = toJSONString(this);
             console.log(loginData);
-            xmlhttp.open("POST", "/login");
+            xmlhttp.open("POST", "/newAccount");
             xmlhttp.setRequestHeader("Content-Type", "application/javascript");
             xmlhttp.send(loginData);
             xmlhttp.onerror = function () { // only triggers if the request couldn't be made at all
                 console.log("ERROR");
             };
+        
             xmlhttp.onload = function () {
                 if (xmlhttp.status != 200) { 
                     alert(xmlhttp.responseText);
                 }
                 else {
                     var newData = JSON.parse(xmlhttp.responseText);
+                    console.log(newData.responseStatus.error);
 
-                    if(newData.responseStatus.error=="USER_NOT_FOUND"){
-                        alert("USER NOT FOUND \n Please make sure you typed in your email and password correctly. \n OR \n If you don't have an account, please create one before logging in.");
-                    }
+                        if(newData.responseStatus.error=="EMAIL_ALREADY_EXISTS")
+                           alert("An account using this email already exists.");
 
-                    else if(newData.responseStatus.error=="")
-                        window.location.href = '/home';
+                        else if(newData.responseStatus.error=="")
+                            window.location.href = '/home';
 
-                    else{
-                        alert(JSON.stringify(newData));
-                        }
+                        else{
+                             alert(JSON.stringify(newData));
+                             }
                 }
             };
+         }
 
         }, false);
 
