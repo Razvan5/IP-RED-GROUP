1facb6b31cef4cee8d76bbcf1bd7cf9b8297f007
Dainsen <martdamian3@yahoo.com>
2020-05-04 14:22:06 +0300

    first-commit

--- public/javascripts/institutionDashBoard.js
+++ public/javascripts/institutionDashBoard.js
@@ -34,8 +34,8 @@ window.onclick = function(event) {
                 } else {
                     var newJson = JSON.parse(xmlhttp.responseText);
                     if (newJson.responseStatus.status === "SUCCESS") {
-                        if (div.parentNode) {
-                            div.parentNode.removeChild(div);
+                        if (div.parentNode.parentNode) {
+                            div.parentNode.parentNode.removeChild(div);
                         }
                     } else {
                         alert(newJson.responseStatus.error);
@@ -43,6 +43,38 @@ window.onclick = function(event) {
                 }
             };
 
+        } else if (target.innerText == "Info") {
+            var div = document.getElementById(target.parentNode.parentNode.parentNode.id);
+            var spans = div.getElementsByTagName("span");
+            var externspan = div.parentNode.getElementsByTagName("span");
+            //  alert(spans[0].innerText);
+            if (externspan.length == 1) {
+                var xmlhttp = new XMLHttpRequest();
+                let data = { institutionName: spans[0].innerText };
+                var myJSON = JSON.stringify(data);
+                data = myJSON;
+                xmlhttp.open("POST", "/institutionDashboard/getInfo");
+                xmlhttp.setRequestHeader("Content-Type", "application/javascript");
+                xmlhttp.send(data);
+                xmlhttp.onerror = function() { // only triggers if the request couldn't be made at all
+                    console.log("ERROR");
+                };
+                xmlhttp.onload = function() {
+                    if (xmlhttp.status != 200) {
+                        alert("NOT WORKING");
+                    } else {
+                        var newJson = JSON.parse(xmlhttp.responseText);
+                        if (newJson.responseStatus.status === "SUCCESS") {
+                            infoHtml(newJson, spans);
+                        } else {
+                            alert(newJson.responseStatus.error);
+                        }
+                    }
+                };
+            } else
+            if (externspan[1].parentNode) {
+                externspan[1].parentNode.removeChild(externspan[1]);
+            }
         } else
             alert(target.innerHTML);
     } else if (!event.target.matches('.dropbtn')) {
@@ -81,11 +113,29 @@ window.onload = function(event) {
 function renderHTML(data) {
     var htmlString = "";
     for (i = 0; i < data.returnedObject.institutions.length; i++) {
-        htmlString += "<li class='institutionItem'> <span>" + data.returnedObject.institutions[i] + "</span> </li> ";
+        htmlString += "<li class='institutionItem'><div> <span>" + data.returnedObject.institutions[i] + "</span></div> </li> ";
     }
     listInstitution[0].insertAdjacentHTML('beforeend', htmlString);
     nowAdd();
 
+};
+
+function infoHtml(data, spanLocation) {
+    var htmlString = "";
+    htmlString += "<span class='institutionItemSpan'>";
+    for (i = 0; i < data.returnedObject.addresses.length; i++) {
+        if (i != 0)
+            htmlString += "<br>";
+        htmlString += "<br>Address " + (i + 1) + "<br>Country:" + data.returnedObject.addresses[i].country + " <br>Region:" + data.returnedObject.addresses[i].region + " <br> City:" + data.returnedObject.addresses[i].city +
+            " <br>Street:" + data.returnedObject.addresses[i].street + " <br>Number:" + data.returnedObject.addresses[i].number + "<br> Building:" + data.returnedObject.addresses[i].building +
+            " <br>Floor:" + data.returnedObject.addresses[i].floor + "<br> Apartment:" + data.returnedObject.addresses[i].apartment + " <br>Main Address:";
+        if (data.returnedObject.addresses[i].isMainAddress == "0")
+            htmlString += "NO";
+        else
+            htmlString += "YES";
+    }
+    htmlString += "</span>";
+    spanLocation[0].parentNode.insertAdjacentHTML('afterend', htmlString);
 };
 
 function nowAdd() {
@@ -94,10 +144,13 @@ function nowAdd() {
     var i;
     for (i = 0; i < myInstitutions.length; i++) {
         var clone = myDropButton.cloneNode(true);
+        var div = myInstitutions[i].getElementsByTagName("div");
         clone.setAttribute("id", i);
         clone.style.display = "block";
         clone.getElementsByClassName("dropbtn")[0].setAttribute("id", i);
-        myInstitutions[i].insertAdjacentElement("afterbegin", clone);
+        div[0].insertAdjacentElement("afterbegin", clone);
+        div[0].setAttribute("id", "institutionItemDiv" + i);
+        div[0].setAttribute("class", "institutionItemDiv");
         myInstitutions[i].setAttribute("id", "institutionItem." + i);
     }
 };
